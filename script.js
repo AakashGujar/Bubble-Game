9d4194235d178455bdba979ccae64ec276b28828
Bexst24 <aakashgujar2001@gmail.com>
2023-08-16 17:14:55 +0530

    Added timer feature

--- script.js
+++ script.js
@@ -1,6 +1,19 @@
-var a = " ";
-for(var i = 0; i<=139; i++){
-    var rn = Math.round(Math.random()* 100)
-    a += `<div class="bubble">${rn}</div>`;
+makebubble = () => {
+    var a = " ";
+    for (var i = 0; i <= 139; i++) {
+        var rn = Math.round(Math.random() * 10)
+        a += `<div class="bubble">${rn}</div>`;
+    }
+    document.querySelector("#pbot").innerHTML = a;
 }
-document.querySelector("#pbot").innerHTML =  a;
+
+var timer = 60;
+function runTimer(){
+    setInterval(function (){
+        timer--;
+        document.querySelector("#timerval").textContent = timer;
+    }, 1000)
+}
+
+runTimer();
+makebubble();
