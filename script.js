03936aaa7b279cfdec59e87fd8d505780bebebed
Bexst24 <aakashgujar2001@gmail.com>
2023-08-16 17:29:21 +0530

    Fixed negative timer

--- script.js
+++ script.js
@@ -1,18 +1,20 @@
 makebubble = () => {
-    var a = " ";
-    for (var i = 0; i <= 139; i++) {
-        var rn = Math.round(Math.random() * 10)
-        a += `<div class="bubble">${rn}</div>`;
-    }
-    document.querySelector("#pbot").innerHTML = a;
-}
+  var a = " ";
+  for (var i = 0; i <= 139; i++) {
+    var rn = Math.round(Math.random() * 10);
+    a += `<div class="bubble">${rn}</div>`;
+  }
+  document.querySelector("#pbot").innerHTML = a;
+};
 
-var timer = 60;
-function runTimer(){
-    setInterval(function (){
-        timer--;
-        document.querySelector("#timerval").textContent = timer;
-    }, 1000)
+var timer = 3;
+function runTimer() {
+  setInterval(function () {
+    if (timer > 0) {
+      timer--;
+      document.querySelector("#timerval").textContent = timer;
+    }
+  }, 1000);
 }
 
 runTimer();
